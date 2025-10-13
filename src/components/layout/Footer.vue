ad8f4d2d09d7a7ba9e3acc25b650f977354cf320
pojangmacha <78676787+pojangmacha@users.noreply.github.com>
2025-10-13 14:23:04 +0700

    Update Footer.vue

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="z-10 border-t-2 border-t-gray-300 bg-gray-100 text-gray-800 dark:border-t-gray-600 dark:bg-blue-900 dark:text-white shadow-md mt-20">
+    <footer class="z-10 border-t-2 border-t-gray-300 bg-gray-100 text-gray-800 dark:border-t-green-600 dark:bg-blue-900 dark:text-white shadow-md mt-20">
         <div class="container p-4 flex justify-between">
              <img 
                 src="@/assets/images/zaelogo.png" 
